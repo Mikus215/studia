eabeb36bb27ba0b493df6fcaa50ee64510121e77
Mikus215 <mikimarsz215@gmail.com>
2022-11-26 19:15:11 +0100

    change from heroku to cyclic

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 axios.defaults.withCredentials = true
 
-const API = axios.create({ baseURL: 'https://opinie-backend.herokuapp.com' })
+const API = axios.create({ baseURL: 'https://kind-swimsuit-crow.cyclic.app/' })
 // const API = axios.create({ baseURL: 'http://localhost:5000/' })
 
 export const registerUser = user => API.post('/user/register', user, { withCredentials: true })
